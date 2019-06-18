b9372a928639edd2c187d20b035a6c1f5db7b4b4
Tillie Smith <tillie@tillies-mbp.home>
2019-06-18 23:19:11 +0100

    Removed most anima stuff, still need to clean up CSS and maybe create a js class to neaten up the functions

--- www/js/userverfication.js
+++ www/js/userverfication.js
@@ -1,10 +1,10 @@
-anima_isHidden = function(e) {
+isHidden = function(e) {
   if (!(e instanceof HTMLElement)) return !1;
   if (getComputedStyle(e).display == "none") return !0;
-  else if (e.parentNode && anima_isHidden(e.parentNode)) return !0;
+  else if (e.parentNode && isHidden(e.parentNode)) return !0;
   return !1;
 };
-anima_loadAsyncSrcForTag = function(tag) {
+loadAsyncSrcForTag = function(tag) {
   var elements = document.getElementsByTagName(tag);
   var toLoad = [];
   for (var i = 0; i < elements.length; i++) {
@@ -18,11 +18,11 @@ anima_loadAsyncSrcForTag = function(tag) {
     if (loaded) continue;
     var asyncSrc = e.getAttribute("anima-src");
     if (asyncSrc == undefined || asyncSrc.length == 0) continue;
-    if (anima_isHidden(e)) continue;
+    if (isHidden(e)) continue;
     toLoad.push(e);
   }
   toLoad.sort(function(a, b) {
-    return anima_getTop(a) - anima_getTop(b);
+    return getTop(a) - getTop(b);
   });
   for (var i = 0; i < toLoad.length; i++) {
     var e = toLoad[i];
@@ -30,7 +30,7 @@ anima_loadAsyncSrcForTag = function(tag) {
     e.setAttribute("src", asyncSrc);
   }
 };
-anima_pauseHiddenVideos = function(tag) {
+pauseHiddenVideos = function(tag) {
   var elements = document.getElementsByTagName("video");
   for (var i = 0; i < elements.length; i++) {
     var e = elements[i];
@@ -40,7 +40,7 @@ anima_pauseHiddenVideos = function(tag) {
       !e.ended &&
       e.readyState > 2
     );
-    var isHidden = anima_isHidden(e);
+    var isHidden = isHidden(e);
     if (!isPlaying && !isHidden && e.getAttribute("autoplay") == "autoplay") {
       e.play();
     } else if (isPlaying && isHidden) {
@@ -48,13 +48,13 @@ anima_pauseHiddenVideos = function(tag) {
     }
   }
 };
-anima_loadAsyncSrc = function(tag) {
-  anima_loadAsyncSrcForTag("img");
-  anima_loadAsyncSrcForTag("iframe");
-  anima_loadAsyncSrcForTag("video");
-  anima_pauseHiddenVideos();
+loadAsyncSrc = function(tag) {
+  loadAsyncSrcForTag("img");
+  loadAsyncSrcForTag("iframe");
+  loadAsyncSrcForTag("video");
+  pauseHiddenVideos();
 };
-var anima_getTop = function(e) {
+var getTop = function(e) {
   var top = 0;
   do {
     top += e.offsetTop || 0;
@@ -62,25 +62,25 @@ var anima_getTop = function(e) {
   } while (e);
   return top;
 };
-anima_loadAsyncSrc();
-anima_old_onResize = window.onresize;
-anima_new_onResize = undefined;
-anima_updateOnResize = function() {
+loadAsyncSrc();
+old_onResize = window.onresize;
+new_onResize = undefined;
+updateOnResize = function() {
   if (
-    anima_new_onResize == undefined ||
-    window.onresize != anima_new_onResize
+    new_onResize == undefined ||
+    window.onresize != new_onResize
   ) {
-    anima_new_onResize = function(x) {
-      if (anima_old_onResize != undefined) anima_old_onResize(x);
-      anima_loadAsyncSrc();
+    new_onResize = function(x) {
+      if (old_onResize != undefined) old_onResize(x);
+      loadAsyncSrc();
     };
-    window.onresize = anima_new_onResize;
+    window.onresize = new_onResize;
     setTimeout(function() {
-      anima_updateOnResize();
+      updateOnResize();
     }, 3000);
   }
 };
-anima_updateOnResize();
+updateOnResize();
 setTimeout(function() {
-  anima_loadAsyncSrc();
+  loadAsyncSrc();
 }, 200);
